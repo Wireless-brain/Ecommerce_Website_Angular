6e16c0ba4cd430202b627f10e0b1d3c37e62a887
AVB <antonybyju4444@email.com>
2024-02-09 23:26:20 +0530

    All set. Added a heading to the home page

--- src/app/elements/home-pg/home-pg.component.ts
+++ src/app/elements/home-pg/home-pg.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { HomeHead2Component } from '../../components/home-head2/home-head2.component';
 
 @Component({
   selector: 'app-home-pg',
   standalone: true,
-  imports: [NavBarComponent,FooterComponent],
+  imports: [NavBarComponent,FooterComponent,HomeHead2Component],
   templateUrl: './home-pg.component.html',
   styleUrl: './home-pg.component.css'
 })
